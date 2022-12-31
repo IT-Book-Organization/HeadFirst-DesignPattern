caa88c95031bf63f73e65fb0d0566dc33a314fb6
JungWoo <rlawjddn103@naver.com>
2022-12-31 19:30:13 +0900

    feat(Chapter2): Chapter2 기능 구현

---
+++ src/main/java/chapter2/DisplayElement.java
@@ -0,0 +1,5 @@
+package main.java.chapter2;
+
+public interface DisplayElement {
+    void display();
+}
---
+++ src/main/java/chapter2/Observer.java
@@ -0,0 +1,5 @@
+package main.java.chapter2;
+
+public interface Observer {
+    void update(float temp, float humidity, float pressure);
+}
