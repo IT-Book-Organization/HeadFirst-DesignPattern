593ab98eaa51da6393ec25a848f9d9548534b9d5
JungWoo <rlawjddn103@naver.com>
2023-01-08 16:50:05 +0900

    feat(Chapter7): 7장 코드 구현

---
+++ src/main/java/chapter7/MallardDuck.java
@@ -0,0 +1,11 @@
+package main.java.chapter7;
+
+public class MallardDuck implements Duck {
+	public void quack() {
+		System.out.println("Quack");
+	}
+ 
+	public void fly() {
+		System.out.println("I'm flying");
+	}
+}
