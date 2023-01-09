2b3b2f043c412d0ca91c64393f31bea321eecf12
JungWoo <rlawjddn103@naver.com>
2023-01-10 02:11:23 +0900

    docs(Chapter9): chapter9 코드 구현

---
+++ src/main/java/chapter9/composite/menu/Waitress.java
@@ -0,0 +1,13 @@
+package main.java.chapter9.composite.menu;
+  
+public class Waitress {
+	MenuComponent allMenus;
+ 
+	public Waitress(MenuComponent allMenus) {
+		this.allMenus = allMenus;
+	}
+ 
+	public void printMenu() {
+		allMenus.print();
+	}
+}
