7ced4e911913facecc0ea24cb3151f8191be4cf2
JungWoo <rlawjddn103@naver.com>
2023-01-11 21:46:50 +0900

    feat(Chapter10): GumballState 구현

---
+++ src/main/java/chapter10/gumballstate/State.java
@@ -0,0 +1,11 @@
+package main.java.chapter10.gumballstate;
+
+public interface State {
+ 
+	public void insertQuarter();
+	public void ejectQuarter();
+	public void turnCrank();
+	public void dispense();
+	
+	public void refill();
+}
