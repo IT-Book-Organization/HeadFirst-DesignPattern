9f44ebe027256246957e0450a7795ae772f6a95c
JungWoo <rlawjddn103@naver.com>
2023-01-17 21:15:56 +0900

    feat(chapter11): gumball 프록시 구현

---
+++ src/main/java/chapter11/gumball/State.java
@@ -0,0 +1,10 @@
+package main.java.chapter11.gumball;
+
+import java.io.*;
+  
+public interface State extends Serializable {
+	public void insertQuarter();
+	public void ejectQuarter();
+	public void turnCrank();
+	public void dispense();
+}
